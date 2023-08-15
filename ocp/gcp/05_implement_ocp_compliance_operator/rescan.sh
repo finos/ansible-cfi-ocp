8a5220c1cf686ca934f0ab59f6d1ec89d00a34ea
Adrian Hammond <adrian.hammond@redhat.com>
2023-08-15 18:44:29 +0100

    added PCI-DSS to OCP on GCP

--- ocp/gcp/05_implement_ocp_compliance_operator/rescan.sh
+++ ocp/gcp/05_implement_ocp_compliance_operator/rescan.sh
@@ -3,3 +3,6 @@
 oc annotate compliancescans/ocp4-cis compliance.openshift.io/rescan=
 oc annotate compliancescans/ocp4-cis-node-master compliance.openshift.io/rescan=
 oc annotate compliancescans/ocp4-cis-node-worker compliance.openshift.io/rescan=
+oc annotate compliancescans/ocp4-pci-dss compliance.openshift.io/rescan=
+oc annotate compliancescans/ocp4-pci-dss-node-master compliance.openshift.io/rescan=
+oc annotate compliancescans/ocp4-pci-dss-node-worker compliance.openshift.io/rescan=
